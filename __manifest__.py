3603fac0535ff09dfa2e040dea19a4a41c4d2dbd
root <root@ip-172-31-26-139.ec2.internal>
2025-01-29 12:48:41 +0000

    ya va

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Transformaciones Gestor',
+    'version': '16.0.1.0.0',
+    'category': 'Tools',
+    'description': 'Este módulo permite gestionar transformaciones de Dragon Ball',
+    'author': 'Yoel Francés & Elena Montserrat',
+    'website': 'https://www.iespacomolla.es',
+    'depends': ['base'],
+    'data': [
+       'views/view_transformaciones.xml',
+       'security/ir.model.access.csv',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
